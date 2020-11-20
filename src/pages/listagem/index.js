55bcf4c639e01af70928c111ce7883a25aa8dc7a
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-11-19 23:03:20 -0300

    ok

--- src/pages/listagem/index.js
+++ src/pages/listagem/index.js
@@ -2,59 +2,57 @@ import React, {useEffect, useState,}from 'react';
 
 import api from '../../services/api';
 import Header from '../../components/header/header';
+import { IoIosHeartEmpty } from "react-icons/io";
 
 import './style.css';
 import '../reset.css';
 
-import RefreshPage from './reload';
-
 export default function Listagem(){
 
   const [photo, setPhotos ] = useState();
-  const [album, setAlbum] = useState();
+  const [album, setAlbum] = useState(0);
 
   useEffect( () => {
     api.get('photos').then(response => {
       setPhotos(response.data);
     });
   }, []);
-
  
   if (!photo) {
     return <p>Carregando...</p>
   }
-
   
   return (
     <div id="listagemConteiner">
       <Header />
 
     <div className="search">
-      <input 
+      <div className="description">
+        <h2>Digite o numero do album que deseja buscar no campo abaixo!</h2>
+      </div>
+      <input
+        type="number"
         id="idAlbum"
         onChange={e => setAlbum(e.target.value)}
         value={album}
-      />
-      
-      <label>{album}</label>
-      <button 
-        type="button"
-        className="atualizar"
-        onClick={RefreshPage}
-      >
-        Atualizar
-      </button>
+        placeholder="Digite o numero do album"
+      />     
+        
     </div>
 
       <div className="album1">     
           <div className="item">
 
-          {photo.filter(id => id.albumId === album).map(filteredAlbum => (
+          {photo.filter(id => id.albumId === +album).map(filteredAlbum => (
             <li key={filteredAlbum.id}>
-              <p>{album}</p>
-              <p>{filteredAlbum.title}</p>
+              <h5 id="album">Album ID : {album}</h5>
+              <h5 id="id">ID da Imagem : {filteredAlbum.id}</h5>
+              <h5 id="title">Title : {filteredAlbum.title}</h5>
+              <div className="icon">
+                <IoIosHeartEmpty size={20} color="#FF669D" />
+              </div>
               <div className="imagem">
-                <img src={filteredAlbum.url} alt={filteredAlbum.title} />
+                <img src={filteredAlbum.thumbnailUrl} alt={filteredAlbum.title} />
               </div>
             </li>
           ))} 
@@ -63,5 +61,5 @@ export default function Listagem(){
       </div>  
 
     </div>
-    )
+  )
 }
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import Inicio from './inicio/index';
 import Listagem from './listagem/index';
-import App from './test';
 
 function Routes () {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Inicio} />
-                <Route path="/lista" exact component={Listagem} />  
-                <Route path="/test" exact component={App} />              
+                
+                <Route path="/" exact component={Listagem} />  
+                             
             </Switch>
         </BrowserRouter>
     );
